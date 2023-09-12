1ff31c8fb1a53be49ee0d212d2050816d9641199
Sushij <sushant.wurk@gmail.com>
2023-09-12 22:22:18 +0530

    tests

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 # from django.template import loader
 
@@ -13,7 +14,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """ Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date=timezone.now()).order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
